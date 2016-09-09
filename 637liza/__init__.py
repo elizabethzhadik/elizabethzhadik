906426b958df827cc1a77d36231497ae69cf503e
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-09 13:26:33 +0300

    tret

---
+++ 637liza/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'student'
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-10, 10.01, 0.01)
+plt.plot(x, x**2)
+plt.show()
+x = input ()
+import numpy as np
+y = np.log(1+x**2,m)
+m= (1/(e**(np.sinx+1))/
